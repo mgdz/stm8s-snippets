815adf2d748b9e73df78cb4fa81320fae0868bc3
Dmitry Bessonov <bessonov.dv@outlook.com>
2018-03-19 14:18:51 +0700

    define macros for lower and higher bytes in word

--- gpio_macro.h
+++ gpio_macro.h
@@ -5,4 +5,7 @@
 #define SET_PIN_LOW(PORT, PIN) { GPIO##PORT->ODR &= (uint8_t)(~(1<<PIN)); }
 #define SET_PIN(PORT, PIN, LEVEL) { SET_PIN_##LEVEL(PORT, PIN); }
 
+#define HI_BYTE(WORD) ((uint8_t)(((WORD) >> 8) & 0xFF))
+#define LO_BYTE(WORD) ((uint8_t)((WORD) & 0xFF))
+
 #endif
